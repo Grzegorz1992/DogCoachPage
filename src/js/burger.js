567d8aa386d7dfd5197945489a367960b025d727
Grzegorz <gkopacki92@gmail.com>
2024-01-12 22:42:03 +0100

    created handler for blocking scroll while mobile menu is on

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,9 +1,16 @@
 const burgerBtn = document.querySelector(".burger");
 const navbarItems = document.querySelector(".navbar-items");
+const body = document.querySelector('body')
+
+
+
+
 
 const showNavbarItems = () => {
 	burgerBtn.classList.toggle("active-burger");
 	navbarItems.classList.toggle("active-navbar-items");
+	body.classList.toggle("disabled-scroll")
 };
 
 burgerBtn.addEventListener("click", showNavbarItems);
+
