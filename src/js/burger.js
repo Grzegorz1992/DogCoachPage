7f0b4440fef5a5d6f4eb8994bb07f8b17f0e4936
Grzegorz <gkopacki92@gmail.com>
2024-01-12 23:08:21 +0100

    added function for hide mobile nav after user click for one of the navbar-items

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,16 +1,18 @@
 const burgerBtn = document.querySelector(".burger");
 const navbarItems = document.querySelector(".navbar-items");
-const body = document.querySelector('body')
-
-
-
-
+const body = document.querySelector("body");
 
 const showNavbarItems = () => {
 	burgerBtn.classList.toggle("active-burger");
 	navbarItems.classList.toggle("active-navbar-items");
-	body.classList.toggle("disabled-scroll")
+	body.classList.toggle("disabled-scroll");
+};
+
+const hideNavbarItems = () => {
+	burgerBtn.classList.remove("active-burger");
+	navbarItems.classList.remove("active-navbar-items");
+	body.classList.remove("disabled-scroll");
 };
 
 burgerBtn.addEventListener("click", showNavbarItems);
-
+navbarItems.addEventListener("click", hideNavbarItems);
